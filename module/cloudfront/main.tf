8683e4b95e974b02d398a6ac89ca563d164da39c
Takayuki Watanabe <watanabe@sikmi.com>
2026-03-30 20:00:03 +0900

    function_associationをデフォルトに変更

--- module/cloudfront/main.tf
+++ module/cloudfront/main.tf
@@ -71,28 +71,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "allow-all"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-  }
-
-  ordered_cache_behavior {
     cached_methods = ["GET", "HEAD"]
     allowed_methods = ["GET", "HEAD"]
-    path_pattern = "/*"
     target_origin_id = local.s3_origin_id
     viewer_protocol_policy = "allow-all"
 
